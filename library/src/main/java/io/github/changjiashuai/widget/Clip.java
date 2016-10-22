30e3761669c1307b16330b7330dfd5e667270b61
changjiashuai <changjiashuai@gmail.com>
2016-10-22 14:32:43 +0800

    Initial commit

---
+++ library/src/main/java/io/github/changjiashuai/widget/Clip.java
@@ -0,0 +1,25 @@
+package io.github.changjiashuai.widget;
+
+import android.content.Context;
+import android.support.v4.widget.Space;
+import android.util.AttributeSet;
+
+/**
+ * Email: changjiashuai@gmail.com
+ *
+ * Created by CJS on 2016/10/22 11:04.
+ */
+
+public class Clip extends Space {
+    public Clip(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public Clip(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Clip(Context context) {
+        super(context);
+    }
+}
